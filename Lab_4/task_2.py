03513c3ccb0627b8b5bd3c6b56e3f6b3b1831bdb
MarinaRakhman1001 <147854714+MarinaRakhman1001@users.noreply.github.com>
2023-11-12 23:43:30 +0300

    Add files via upload

---
+++ Lab_4/task_2.py
@@ -0,0 +1,20 @@
+import csv
+import json
+
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+
+def task() -> None:
+    with open(INPUT_FILENAME, encoding='utf-8') as csvf:
+        lines = [line for line in csv.DictReader(csvf)]
+
+    with open(OUTPUT_FILENAME, "w", encoding="utf-8") as output_file:
+        json.dump(lines, output_file, indent=4)
+
+if __name__ == '__main__':
+    # Нужно для проверки
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
